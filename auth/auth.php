2227a98c5282cbb808c9aa07ed5e8bef33875ed8
daranovia <148682410+daranovia@users.noreply.github.com>
2024-09-23 19:42:26 +0700

    Update auth.php

--- auth/auth.php
+++ auth/auth.php
@@ -2,7 +2,9 @@
 
 session_start();
 include "../koneksi.php";
+function login($username, $password){
 
+}
 if(isset($_POST['login'])){
     $username=$_POST['username'];
     $password=$_POST['password'];
@@ -24,5 +26,23 @@ if(isset($_POST['login'])){
     }
 }
 
+function register($username, $email, $password){
+    
+}
+if (isset($_POST['register'])) {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $query = "INSERT INTO daftar (`username`, `email`, `password`) VALUES ('$username', '$email', '$password')";
+    $result = mysqli_query($koneksi, $query);
+
+    if ($result) {
+        header("location:login.php");
+        exit();
+    } else {
+        echo "Pendaftaran Gagal: " . mysqli_error($koneksi);
+    }
+}
 
 ?>
